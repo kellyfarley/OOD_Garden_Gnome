038aae039d34e09d1923dab26204f0b9b2729d88
Kelly Farley <farleykc@gmail.com>
2016-02-29 14:24:47 -0600

    0 failures

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,52 @@
 # Code your instances here
+
+class GardenGnome
+	def initialize(personality="evil", hat_color="red")
+			@personality = personality
+			@hat_color = hat_color
+	end
+
+	def personality
+		return @personality
+	end
+
+	def name=(name)
+		@name = name
+	end
+
+	def name
+		return @name
+	end
+
+	def age=(age)
+		@age = age
+	end
+
+	def age
+		return @age
+	end
+
+	def gluten_allergy=(gluten_allergy)
+		@gluten_allergy = gluten_allergy
+	end
+
+	def gluten_allergy
+		return @gluten_allergy
+	end
+
+	def gnaw
+		return "Gnawing on a tree!!!"
+	end
+
+	def shout
+		return "GNARLY!!!"
+	end
+
+	def hat_color
+		return @hat_color
+	end
+
+	def introduce_self
+		return "Hello humans, my name is #{@name}, I am #{@age} years old, and you'll rue the day you crossed me!"
+	end
+end
